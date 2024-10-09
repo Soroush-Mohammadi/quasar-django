78415fe78e94fbc02b1ee23215ab64ef522e8fa2
soroosh <mohamadysoroush@gmail.com>
2024-10-09 15:50:23 +0330

    add cartView responsive

--- src/pages/CartView.vue
+++ src/pages/CartView.vue
@@ -1,44 +1,55 @@
 <template>
-  <ul class="row bg-white q-pa-md">
+  <ul class="row bg-white flex-center">
     <div class="col-xs-12 bg-blue">
       <h5>Shop Cart</h5>
     </div>
-    <div class="col-xs-12 bg-red">
+    <div class="col-xs-12 col-md-8 bg-teal-2 offset-md-2">
       <li
         v-for="item in cart"
         :key="item.id"
         class="q-pa-md row justify-center items-center"
       >
-        <div class="col-xs-12 flex justify-center bg-cyan-1">
+        <div class="col-xs-12 col-lg-3 flex justify-center bg-cyan-1">
           <img
             :src="`${baseUrl}${item.image}`"
             alt=""
-            style="border: 1px solid black"
+            style="border: 1px solid black; width: 100%"
           />
         </div>
         <div
-          class="q-px-md flex justify-center items-center col-xs-12 bg-cyan-2"
+          class="q-px-md flex justify-center items-center col-xs-12 col-lg-3 bg-cyan-2"
         >
-          <h5 class="q-px-sm">Title :</h5>
+          <h5 class="q-px-sm lt-md">Title :</h5>
           <h6>{{ item.name }}</h6>
         </div>
 
-        <div class="col-xs-12 flex justify-center items-center bg-cyan-4">
+        <div
+          class="col-xs-12 col-lg-3 flex justify-center items-center bg-cyan-1"
+        >
           <h5>Price :</h5>
           <h6 class="q-mx-md">{{ item.totalPrice }}$</h6>
         </div>
-        <div
-          class="col-xs-12 flex justify-between bg-cyan-6"
-          style="width: 200px"
-        >
-          <button @click="removeItemFromCart(item)" class="q-px-md">-</button>
+        <div class="col-xs-12 col-md-5 col-lg-3 flex justify-around bg-cyan-1">
+          <q-btn
+            style="width: 100px"
+            @click="removeItemFromCart(item)"
+            class="q-px-md"
+            color="red-7"
+            >-</q-btn
+          >
           <span>{{ item.quantity }}</span>
-          <button @click="addToCart(item)" class="q-px-md">+</button>
+          <q-btn
+            color="green-7"
+            style="width: 100px"
+            @click="addToCart(item)"
+            class="q-px-md"
+            >+</q-btn
+          >
         </div>
       </li>
     </div>
     <hr />
-    <div class="col-xs-12 bg-green">
+    <div class="col-xs-12 col-md-8 bg-green q-pa-md">
       <li class="items-center col" style="list-style: none; width: 100%">
         <h3>Total Price :</h3>
         <h6>{{ store.calculateTotal() }}$</h6>
@@ -136,5 +147,3 @@ export default {
   },
 };
 </script>
-
-<style scoped></style>
